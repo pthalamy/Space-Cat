00bc179794e1c7292330e4b91949ab532b75275a
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-07-27 19:21:21 +0200

    Create a new class for constants and utils methods

---
+++ Space Cat/PTHYConstants.h
@@ -0,0 +1,12 @@
+//
+//  PTHYConstants.h
+//  Space Cat
+//
+//  Created by Pierre Thalamy on 27/07/14.
+//  Copyright (c) 2014 Pierre Thalamy. All rights reserved.
+//
+
+#ifndef Space_Cat_PTHYConstants_h
+#define Space_Cat_PTHYConstants_h
+
+#endif
---
+++ Space Cat/PTHYUtils.h
@@ -0,0 +1,15 @@
+//
+//  PTHYUtils.h
+//  Space Cat
+//
+//  Created by Pierre Thalamy on 27/07/14.
+//  Copyright (c) 2014 Pierre Thalamy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static const int PTHYProjectileSpeed = 400;
+
+@interface PTHYUtils : NSObject
+
+@end
